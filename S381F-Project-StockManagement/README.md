46cfb25cb310625f7e4b343a406ea750baec2ed2
SeReN3247 <88083927+SeReN3247@users.noreply.github.com>
2023-11-20 18:57:01 +0800

    Add files via upload

---
+++ S381F-Project-StockManagement/README.md
@@ -0,0 +1,97 @@
+Stock management system
+
+Group: 84
+Name: LIN, Jiacheng (13124347)
+Application link: https://stockmanagement-lwf6.onrender.com
+
+********************************************
+# Login
+Through the login interface, each user can access the stock management system by entering their username and password.
+
+Each user has a userID and password;
+[
+	{userid: user1, password: us123},
+	{userid: user2, password: us123},
+	{adminid: user3, password: sp987}
+
+]
+
+After successful login, userid is stored in seesion.
+
+********************************************
+# Logout
+In the home page, each user can log out their account by clicking logout.
+
+********************************************
+# CRUD service
+- Create
+-	A restaurant document may contain the following attributes with an example: 
+	1)	Item Name (M&M Chocolate)
+	2)	Item ID (0001), item id must be 4 digits
+	3)	Manufacturer (M&M)
+	4)	Manufacturer Contact (22544579), telephone number must be 8 digits
+	5)	Description (classic M&M chocolate, 70g, small package)
+
+Item Name and Item ID is mandatory, and other attributes are optional.
+
+Create operation is post request, and all information is in body of request.
+
+********************************************
+# CRUD service
+- Read
+- There are two options to read and find all information of item list or searching by item id.
+
+1) List all information
+	display.ejs will be displayed with all item ID
+	clicking on item ID, the details of the tiem will be shown
+
+2) Searching by item id
+	input the id of any item you want to find (0001)
+	id is in the body of post request, and in display.ejs item id will be shown as link
+	clicking on item ID, the details of the tiem will be displayed
+
+********************************************
+# CRUD service
+- Update
+- The user can update the item information through the details interface.
+- Among the attribute shown above, Item ID cannot be changed. Since Item ID is fixed, Item ID is searching criteria for updating information. 
+
+- A item document may contain the following attributes with an example: 
+	1)	Item Name (M&M Chocolate (small package))
+	2)	Manufacturer (M&M)
+	3)	Manufacturer Contact (27931322), telephone number must be 8 digits
+	4)	Description (classic M&M chocolate, 70g, small package)	
+
+	In example, I updated the item name and manufacturer contact number.
+
+********************************************
+# CRUD service
+- Delete
+-	The user can delete the item information through the details interface.
+
+********************************************
+# Restful
+In this project, there are three HTTP request types, post, get and delete.
+- Post 
+	Post request is used for insert.
+	Path URL: /api/item/itemID/:itemID
+	Test: curl -X POST -H "Content-Type: application/json" --data '{"name": "Coca Cola", "itemID":"0002"}' localhost:8099/api/item/itemID/0002/name/Coca Cola
+
+- Get
+	Get request is used for find.
+	Path URL: /api/item/itemID/:itemID
+	Test: curl -X GET http://localhost:8099/api/item/itemID/0003
+
+- Delete
+	Delete request is used for deletion.
+	Path URL: /api/item/itemID/:itemID
+	Test: curl -X DELETE localhost:8099/api/item/itemID/0003
+
+Login should be done at first for all restful CRUD services.
+
+
+curl -X POST -H "Content-Type: application/json" --data '{"name": "Coca Cola", "itemID":"0002"}' http://localhost:8099/api/item/itemID/0002
+
+curl -X GET http://localhost:8099/api/item/itemID/0003
+
+curl -X DELETE http://localhost:8099/api/item/itemID/0003
---
+++ S381F-Project-StockManagement/server.js
@@ -0,0 +1,397 @@
+const assert = require('assert');
+
+const MongoClient = require('mongodb').MongoClient;
+const ObjectID = require('mongodb').ObjectID;
+
+const mongourl = 'mongodb+srv://KasenLin:Bjklsad123@stockmanagement.otjjmtt.mongodb.net/?retryWrites=true&w=majority'; 
+const dbName = 'StockManagement';
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const session = require('cookie-session');
+const SECRETKEY = 'cs381';
+
+var usersinfo = new Array(
+    	{userid: user1, password: us123},
+	{userid: user2, password: us123},
+	{adminid: user3, password: sp987}
+);
+
+var documents = {};
+//Main Body
+app.set('view engine', 'ejs');
+app.use(session({
+    userid: "session",  
+    keys: [SECRETKEY],
+}));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+const createDocument = function(db, createddocuments, callback){
+    const client = new MongoClient(mongourl);
+    client.connect(function(err) {
+        assert.equal(null, err);
+        console.log("Connected successfully to the MongoDB database server.");
+        const db = client.db(dbName);
+
+        db.collection('Item').insertOne(createddocuments, function(error, results){
+            if(error){
+            	throw error
+            };
+            console.log(results);
+            return callback();
+        });
+    });
+}
+
+const findDocument =  function(db, criteria, callback){
+    let cursor = db.collection('Item').find(criteria);
+    console.log(`findDocument: ${JSON.stringify(criteria)}`);
+    cursor.toArray(function(err, docs){
+        assert.equal(err, null);
+        console.log(`findDocument: ${docs.length}`);
+        return callback(docs);
+    });
+}
+
+const handle_Find = function(res, criteria){
+    const client = new MongoClient(mongourl);
+    client.connect(function(err) {
+        assert.equal(null, err);
+        console.log("Connected successfully to server");
+        const db = client.db(dbName);
+
+        findDocument(db, criteria, function(docs){
+            client.close();
+            console.log("Closed DB connection");
+            res.status(200).render('display', {nItems: docs.length, items: docs});
+        });
+    });
+}
+
+const handle_Edit = function(res, criteria) {
+    const client = new MongoClient(mongourl);
+    client.connect(function(err) {
+        assert.equal(null, err);
+        console.log("Connected successfully to server");
+        const db = client.db(dbName);
+
+        let documentID = {};
+        documentID['_id'] = ObjectID(criteria._id)
+        let cursor = db.collection('Item').find(documentID);
+        cursor.toArray(function(err,docs) {
+            client.close();
+            assert.equal(err,null);
+            res.status(200).render('edit',{item: docs[0]});
+
+        });
+    });
+}
+
+const handle_Details = function(res, criteria) {
+    const client = new MongoClient(mongourl);
+    client.connect(function(err) {
+        assert.equal(null, err);
+        console.log("Connected successfully to server");
+        const db = client.db(dbName);
+
+        let documentID = {};
+        documentID['_id'] = ObjectID(criteria._id)
+        findDocument(db, documentID, function(docs){ 
+            client.close();
+            console.log("Closed DB connection");
+            res.status(200).render('details', {item: docs[0]});
+        });
+    });
+}
+
+const updateDocument = function(criteria, updatedocument, callback){
+    const client = new MongoClient(mongourl);
+    client.connect(function(err){
+        assert.equal(null, err);
+        console.log("Connected successfully to server");
+        const db = client.db(dbName);
+        console.log(criteria);
+	console.log(updatedocument);
+	
+        db.collection('Item').updateOne(criteria,{
+                $set: updatedocument
+            }, function(err, results){
+                client.close();
+                assert.equal(err, null);
+                return callback(results);
+            }
+        );
+    });
+}
+
+const deleteDocument = function(db, criteria, callback){
+console.log(criteria);
+	db.collection('Item').deleteOne(criteria, function(err, results){
+	assert.equal(err, null);
+	console.log(results);
+	return callback();
+	});
+
+};
+
+const handle_Delete = function(res, criteria) {
+    const client = new MongoClient(mongourl);
+    client.connect(function(err) {
+        console.log("Connected successfully to server");
+        const db = client.db(dbName);
+	
+	let deldocument = {};
+	
+        deldocument["_id"] = ObjectID(criteria._id);
+        deldocument["ownerID"] = criteria.owner;
+        console.log(deldocument["_id"]);
+        console.log(deldocument["ownerID"]);
+        
+        deleteDocument(db, deldocument, function(results){
+            client.close();
+            console.log("Closed DB connection");
+            res.status(200).render('info', {message: "Document is successfully deleted."});
+        })     
+    });
+    //client.close();
+    //res.status(200).render('info', {message: "Document is successfully deleted."});
+}
+
+app.get('/', function(req, res){
+    if(!req.session.authenticated){
+        console.log("...Not authenticated; directing to login");
+        res.redirect("/login");
+    }else{
+    	res.redirect("/login");
+    }
+    console.log("...Hello, welcome back");
+});
+
+//login
+app.get('/login', function(req, res){
+    console.log("...Welcome to login page.")
+    res.sendFile(__dirname + '/public/login.html');
+    return res.status(200).render("login");
+});
+
+app.post('/login', function(req, res){
+    console.log("...Handling your login request");
+    for (var i=0; i<usersinfo.length; i++){
+        if (usersinfo[i].name == req.body.username && usersinfo[i].password == req.body.password) {
+        req.session.authenticated = true;
+        req.session.userid = usersinfo[i].name;
+        console.log(req.session.userid);
+        return res.status(200).redirect("/home");
+        }
+    }
+        console.log("Error username or password.");
+        return res.redirect("/");
+});
+
+app.get('/logout', function(req, res){
+    req.session = null;
+    req.authenticated = false;
+    res.redirect('/login');
+});
+
+app.get('/home', function(req, res){
+    console.log("...Welcome to the home page!");
+    return res.status(200).render("home");
+});
+
+app.get('/list', function(req, res){
+    console.log("Show all information! ");
+    handle_Find(res,req.query.docs);
+    
+});
+
+app.get('/find', function(req, res){
+    return res.status(200).render("search");
+});
+
+app.post('/search', function(req, res){
+    const client = new MongoClient(mongourl);
+    client.connect(function(err){
+        assert.equal(null, err);
+        console.log("Connected successfully to the DB server.");
+        const db = client.db(dbName);
+    
+    var searchID={};
+    searchID['itemID'] = req.body.itemID;
+    
+    if (searchID.itemID){
+    console.log("...Searching the document");
+    findDocument(db, searchID, function(docs){
+            client.close();
+            console.log("Closed DB connection");
+            res.status(200).render('display', {nItems: docs.length, items: docs});
+        });
+    }
+    else{
+    console.log("Invalid Entry - Item ID is compulsory for searching!");
+    res.status(200).redirect('/find');
+    }         	
+	});
+});
+
+app.get('/details', function(req,res){
+    handle_Details(res, req.query);
+});
+
+app.get('/edit', function(req,res) {
+    handle_Edit(res, req.query);
+})
+
+app.get('/create', function(req, res){
+    return res.status(200).render("create");
+});
+
+app.post('/create', function(req, res){
+    const client = new MongoClient(mongourl);
+    client.connect(function(err){
+        assert.equal(null, err);
+        console.log("Connected successfully to the DB server.");
+        const db = client.db(dbName);
+        
+        documents["_id"] = ObjectID;        
+	documents["itemID"] = req.body.itemID;	
+	documents['name']= req.body.name;
+	documents['manufacturer']= req.body.manufacturer;
+	documents['phone']= req.body.number;
+        documents['description']= req.body.description;
+        
+        documents["ownerID"] = `${req.session.userid}`;
+        
+        if(documents.itemID){
+            console.log("...Creating the document");
+            createDocument(db, documents, function(docs){
+                client.close();
+                console.log("Closed DB connection");
+                return res.status(200).render('info', {message: "Document is created successfully!"});
+            });
+        } else{
+            client.close();
+            console.log("Closed DB connection");
+            return res.status(200).render('info', {message: "Invalid entry - Item ID is compulsory!"});
+        }
+    });
+    //client.close();
+    //return res.status(200).render('info', {message: "Document created"}); 
+});
+
+
+app.post('/update', function(req, res){
+    var updatedocument={};
+    const client = new MongoClient(mongourl);
+        client.connect(function(err){
+            assert.equal(null, err);
+            console.log("Connected successfully to server");
+            
+                if(req.body.name){
+                updatedocument["ownerID"] = `${req.session.userid}`
+                updatedocument['name']= req.body.name;
+                updatedocument['manufacturer']= req.body.manufacturer;
+                updatedocument['phone']= req.body.number;
+                updatedocument['description']= req.body.description;
+
+        	let updateDoc = {};
+                updateDoc['itemID'] = req.body.postId;
+                console.log(updateDoc);
+
+                updateDocument(updateDoc, updatedocument, function(docs) {
+                    client.close();
+                    console.log("Closed DB connection");
+                    return res.render('info', {message: "Document is updated successfully!."});
+                    
+                })
+            }
+            else{
+            	return res.render('info', {message: "Invalid entry - Item name is compulsory!"});
+            }
+    });
+    
+});
+
+app.get('/delete', function(req, res){
+    if(req.query.owner == req.session.userid){
+        console.log("...Hello !");
+        handle_Delete(res, req.query);
+    }else{
+        return res.status(200).render('info', {message: "Access denied - You don't have the access and deletion right!"}); 
+    }
+});
+
+//Restful
+//insert
+app.post('/api/item/itemID/:itemID', function(req,res) {
+    if (req.params.itemID) {
+        console.log(req.body)
+        const client = new MongoClient(mongourl);
+        client.connect(function(err){
+            assert.equal(null,err);
+            console.log("Connected successfully to server");
+            const db = client.db(dbName);
+            let newDocument = {};
+            newDocument['itemID'] = req.body.itemID;
+
+   	db.collection('Item').insertOne(newDocument, function(err,results){
+                assert.equal(err,null);
+                client.close()
+                res.status(200).end()
+                    });
+          
+                })
+            }
+        else {
+        res.status(500).json({"error": "missing item ID"});
+    }
+})
+
+//find
+app.get('/api/item/itemID/:itemID', function(req,res) {
+    if (req.params.itemID) {
+        let criteria = {};
+        criteria['itemID'] = req.params.itemID;
+        const client = new MongoClient(mongourl);
+        client.connect(function(err) {
+            assert.equal(null, err);
+            console.log("Connected successfully to server");
+            const db = client.db(dbName);
+
+            findDocument(db, criteria, function(docs){
+                client.close();
+                console.log("Closed DB connection");
+                res.status(200).json(docs);
+            });
+        });
+    } else {
+        res.status(500).json({"error": "missing item id"});
+    }
+})
+
+//delete
+app.delete('/api/item/itemID/:itemID', function(req,res){
+    if (req.params.itemID) {
+        let criteria = {};
+        criteria['itemID'] = req.params.itemID;
+        const client = new MongoClient(mongourl);
+        client.connect(function(err){
+            assert.equal(null, err);
+            console.log("Connected successfully to server");
+            const db = client.db(dbName);
+
+            db.collection('Item').deleteMany(criteria, function(err,results) {
+                assert.equal(err,null)
+                client.close()
+                res.status(200).end();
+            })
+        });
+    } else {
+        res.status(500).json({"error": "missing item id"});       
+    }
+})
+
+app.listen(app.listen(process.env.PORT || 8099));
